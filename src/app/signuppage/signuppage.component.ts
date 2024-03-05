5531f7eeb767b89c8a6e0c192d9d021c6e5945aa
[opadeji mary] <feranmiopadeji@gmail.com>
2024-03-05 13:43:55 +0100

    signup

---
+++ src/app/signuppage/signuppage.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Routes } from '@angular/router';
+
+@Component({
+  selector: 'app-signuppage',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './signuppage.component.html',
+  styleUrl: './signuppage.component.css'
+})
+export class SignuppageComponent {
+
+}
